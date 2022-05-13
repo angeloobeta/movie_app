88e5c799c3451ce07bfb20f195e626dd814f5e52
Obeta Ifeanyichukwu Malachy <angeloobeta@hotmail.com>
2022-05-13 18:41:49 +0100

    refactor code

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:movie_app/screen/screen1.dart';
-import 'package:movie_app/screen/screen2.dart';
-import 'package:movie_app/screen/screen3.dart';
-import 'package:movie_app/screen/screen4.dart';
+import 'package:movie_app/home_screen.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Screen1(),
-    routes: <String, WidgetBuilder>{
-      '/screen1': (BuildContext context) => Screen1(),
-      '/screen2': (BuildContext context) => Screen2(),
-      '/screen3': (BuildContext context) => Screen3(),
-      '/screen4': (BuildContext context) => Screen4()
-    },
+    home: HomeScreen(),
   ));
   print("Whatever you want to do is okay");
 }
-
-// https://tutsnode.net/php-registration-form-email-verification-responsive-email/
