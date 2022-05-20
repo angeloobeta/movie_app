bff5c8748b352cc2189d6804f0153a389228b2a2
Obeta Ifeanyichukwu Malachy <angeloobeta@hotmail.com>
2022-05-20 21:05:23 +0100

    Introduce: Dartz Plugin. Right(for the response) & Left(for the error message).

--- lib/domain/use_cases/get_trending.dart
+++ lib/domain/use_cases/get_trending.dart
@@ -7,7 +7,7 @@ class GetTrending {
   final MovieRepository repository;
   GetTrending(this.repository);
 
-  Future<Either<AppError, List<MovieEntity>?>?> call() async {
+  Future<Either<AppError, List<MovieEntity>?>> call() async {
     return await repository.getTrending();
   }
 }
