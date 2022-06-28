4406aacd86680fbf279894b5af17b8f4ab99afc8
Obeta Ifeanyichukwu Malachy <angeloobeta@hotmail.com>
2022-06-28 18:34:55 +0100

    Added more files from a formal project

---
+++ local_data_test_case/lib/blocs/product/product_bloc.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:pypicker/model/product.dart';
+import 'package:pypicker/repository/product_repository.dart';
+
+part 'product_event.dart';
+part 'product_state.dart';
+
+class ProductBloc extends Bloc<ProductEvent, ProductState> {
+  final ProductRepository? repository;
+
+  ProductBloc({this.repository}) : super(ProductInitial()) {
+    on<GetProductEvent>(_mapGetProductToState);
+    on<GetFavouriteProductEvent>(_mapGetFavouriteProductEventToState);
+    on<SaveFavouriteProductEvent>(_mapSaveFavouriteProductEventToState);
+  }
+
+  FutureOr<void> _mapGetProductToState(
+      GetProductEvent event, Emitter<ProductState> emit) async {
+    emit(ProductLoadingState());
+    try {
+      List<Product> products = await repository!.getProducts();
+      emit(ProductLoadedState(products));
+    } catch (e) {
+      emit(ProductErrorState(e.toString()));
+    }
+  }
+
+  FutureOr<void> _mapGetFavouriteProductEventToState(
+      GetFavouriteProductEvent event, Emitter<ProductState> emit) async {
+    emit(ProductLoadingState());
+    try {
+      List<Product> products = await repository!.getFavouriteProduct();
+      emit(ProductLoadedState(products));
+    } catch (e) {
+      emit(ProductErrorState(e.toString()));
+    }
+  }
+
+  FutureOr<void> _mapSaveFavouriteProductEventToState(
+      SaveFavouriteProductEvent event, Emitter<ProductState> emit) async {
+    emit(FavouriteProductLoadingState());
+    try {
+      await repository!.saveFavouriteProduct(event.product);
+      emit(ProductSavedState());
+    } catch (e) {
+      emit(FavouriteProductErrorState(e.toString()));
+    }
+  }
+}
---
+++ local_data_test_case/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:pypicker/view/bottom_navigation_screen.dart';
+
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BottomNavigationScreen(),
+    ));
