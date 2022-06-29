3ec8d8be0157a49306c79fac211a427e757ef179
Obeta Ifeanyichukwu Malachy <angeloobeta@hotmail.com>
2022-06-29 20:50:35 +0100

    Refactoring

--- local_data_test_case/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:pypicker/view/bottom_navigation_screen.dart';
-
-void main() => runApp(const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: BottomNavigationScreen(),
-    ));
