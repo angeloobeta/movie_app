710f687e14bf00e2b7bb93fab60cedf9cd375a60
Obeta Ifeanyichukwu Malachy <angeloobeta@hotmail.com>
2022-07-01 18:51:18 +0100

    Added the flutter binding engine for the framework and some initialisation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,16 @@
 // import 'domain/repositories/movie_repository.dart';
 
-void main() {}
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:pedantic/pedantic.dart';
+
+void main() {
+  // Based on the official flutter documentation
+//  The glue that binds the flutter framework and the flutter engine
+  WidgetsFlutterBinding.ensureInitialized();
+  unawaited(
+      SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]));
+  // Initialize GetIt to provide us with dependencies
+  unawaited(getIt.init());
+  runApp(movieApp());
+}
