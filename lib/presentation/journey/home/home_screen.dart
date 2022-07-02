fa1369e951ab89053e1cfb8355574d8dbb89567e
Obeta Ifeanyichukwu Malachy <angeloobeta@hotmail.com>
2022-07-02 16:38:29 +0100

    Add the Logo Class  ==> presentation/widget/logo.dart

--- lib/presentation/journey/home/home_screen.dart
+++ lib/presentation/journey/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:movie_app/dependency_injection/get_it.dart';
 import 'package:movie_app/presentation/blocs/movie_carousel/movie_carousel_bloc.dart';
 
@@ -28,6 +29,28 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // The top part is 60% of the fractional screen while the bottom is 40% fractional of the screen
+    return Scaffold(
+      body: Stack(
+        // This allows the stack to take all the availsable space
+        fit: StackFit.expand,
+        children: const [
+          FractionallySizedBox(
+            alignment: Alignment.topCenter,
+            heightFactor: 0.6,
+            child: Placeholder(
+              color: Colors.grey,
+            ),
+          ),
+          FractionallySizedBox(
+            alignment: Alignment.topCenter,
+            heightFactor: 0.4,
+            child: Placeholder(
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/presentation/widget/logo.dar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Logo extends StatefulWidget {
+  const Logo({Key? key}) : super(key: key);
+
+  @override
+  State<Logo> createState() => _LogoState();
+}
+
+class _LogoState extends State<Logo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
