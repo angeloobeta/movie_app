7ba1e5e475cf41f864c9d5142fc2b8a63f2aa22f
Obeta Ifeanyichukwu Malachy <angeloobeta@hotmail.com>
2022-07-02 18:51:48 +0100

    Added MovieCardWidget ==> presentation/journey/home/movie_carousel/movie_card_widget.dart

---
+++ lib/presentation/journey/home/movie_carousel/movie_card_widget.dart
@@ -0,0 +1,33 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:movie_app/common/constants/size_constants.dart';
+import 'package:movie_app/data/core/api_constants.dart';
+
+class MovieCardWidget extends StatelessWidget {
+  final int movieId;
+  final String postalPath;
+
+  const MovieCardWidget(
+      {Key? key, required this.movieId, required this.postalPath})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      borderRadius: BorderRadius.circular(Sizes.dimen_16.w),
+      elevation: 32,
+      child: GestureDetector(
+        onTap: () {},
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(Sizes.dimen_16.w),
+          child: CachedNetworkImage(
+            imageUrl: '${ApiConstants.BASE_IMAGE_URL}$postalPath)',
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
